3b9943d426f009563dbcd9b3a80716ef574af1f7
Kaninika Sen <kaninikasc@gmail.com>
2024-04-04 21:17:59 +0530

    grid updated

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -1,8 +1,8 @@
 "use client";
+import { PokemonCard } from "./pokemon-card";
 import { useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import { PokemonCard } from "./pokemon-card";
 
 interface PokemonGridProps {
   pokemonList: any;
@@ -11,38 +11,44 @@ interface PokemonGridProps {
 export function PokemonGrid({ pokemonList }: PokemonGridProps) {
   const [searchText, setSearchText] = useState("");
 
-  //console.log(pokemonList);
+  console.log(pokemonList);
+
   const searchFilter = (pokemonList: any) => {
     return pokemonList.filter((pokemon: any) =>
       pokemon.name.toLowerCase().includes(searchText.toLowerCase())
     );
   };
-  //saving
+
+  // save the filtered array of objects
   const filteredPokemonList = searchFilter(pokemonList);
+
+  // show the filtered array to user
 
   return (
     <>
       <div>
-        <h3 className="text-2xl py-6 text-center"> Search For your Pokemon!</h3>
+        <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="pokemonName">Pokemon Name</Label>
           <Input
             type="text"
-            id="pokemonName"
-            autoComplete="off"
-            placeholder="pika pika :p"
             value={searchText}
+            autoComplete="off"
+            id="pokemonName"
+            placeholder="Charizard, Pikachu, etc."
             onChange={(e) => setSearchText(e.target.value)}
           />
         </div>
-        <h3 className="text-xl pt-12"> {} </h3>
+        <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
       </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
         {filteredPokemonList.map((pokemon: any) => {
-          return <PokemonCard name={pokemon.name} />;
+          return (
+            <PokemonCard name={pokemon.name} key={pokemon.name + "Card"} />
+          );
         })}
       </div>
     </>
   );
 }
-//task: // filter the text + save the filtered array
